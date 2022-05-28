737c2ed20b2cca0aa61aa5b1ee96f94af10d4eff
zhangtao615 <295564283@qq.com>
2022-05-28 13:17:24 +0800

    选择排序实现测试随机数数组的性能

--- Chapter1/LinearSearch/Search/ArrayGenerator.go
+++ Chapter1/LinearSearch/Search/ArrayGenerator.go
@@ -1,9 +1,20 @@
 package Search
 
+import "math/rand"
+
 func GeneratorOrderedArray(n int) []int {
 	var res []int
 	for i := 0; i < n; i++ {
 		res = append(res, i)
+	}
+
+	return res
+}
+
+func RandomArray(n int) []int {
+	var res []int
+	for i := 0; i < n; i++ {
+		res = append(res, rand.Intn(100000))
 	}
 
 	return res
--- Chapter1/Sort/main.go
+++ Chapter1/Sort/main.go
@@ -2,13 +2,23 @@
 package main
 
 import (
+	"algorithmSystem/Chapter1/LinearSearch/Search"
 	"algorithmSystem/Chapter1/Sort/SortDict"
 	"fmt"
+	"time"
 )
 
 func main() {
 	//var nums = []int{1, 5, 4, 7, 3, 9}
-	var arr = []string{"apple", "abandon", "c", "b", "d", "w", "z"}
+	var arr = Search.RandomArray(10000)
+	start := time.Now()
 	SortDict.SelectionSort(arr)
-	fmt.Println(arr) // [abandon apple b c d w z]
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < arr[i-1] {
+			fmt.Println("未排序成功")
+			return
+		}
+	}
+	end := time.Since(start)
+	fmt.Println("排序成功, 排序花费时间为：", end)
 }
